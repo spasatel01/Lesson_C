20a2801ebb21f448b0ea03050f5c03ff8403be18
Alexandr Spasskii <saa84@list.ru>
2023-02-14 19:04:50 +0300

    add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число.");
+
+int inValue = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number <= inValue)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number++;
+}
