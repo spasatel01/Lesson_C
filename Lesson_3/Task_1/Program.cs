a4f793ec9c1128ecb9c1f6c77a6f7a66bfd532d2
Alexandr Spasskii <saa84@list.ru>
2023-02-24 11:14:24 +0300

    add

---
+++ Lesson_3/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage}");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
