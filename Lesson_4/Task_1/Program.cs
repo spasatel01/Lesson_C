05a6ddcc2e8500d7cc11f059cc7ab25b22c47b65
Alexandr Spasskii <saa84@list.ru>
2023-02-26 21:53:21 +0300

    add Lesson_4

---
+++ Lesson_4/Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int DegreeNumbers(int numberOne, int numberTwo)
+{
+    int degree = numberOne;
+
+    for (int i = 1; i < numberTwo; i++)
+    {
+        degree = degree * numberOne;
+    }
+    return degree;
+}
+
+int numberOne = InputInt("Ведите первое число.");
+int numberTwo = InputInt("Ведите второе число.");
+
+Console.WriteLine($"Первое введеное число в степени второго введеного числа равно {DegreeNumbers(numberOne, numberTwo)}.");
---
+++ Lesson_4/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int number)
+{
+    int sumNumber = 0;
+    while (number > 0)
+    {
+        int num = number % 10;
+        number = number / 10;
+        sumNumber = sumNumber + num;
+    }
+    return sumNumber;
+}
+
+int number = InputInt("Введите целое число.");
+
+Console.WriteLine($"Cумма всех цифр в введенном числе равна: {SumNumbers(number)}.");
